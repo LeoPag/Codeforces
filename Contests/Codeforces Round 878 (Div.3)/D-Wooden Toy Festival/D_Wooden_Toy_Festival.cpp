e8a1a6a9719d747132f3dd46a06bd32ee18db564
lpagan <leonardo.pagani99@gmail.com>
2023-07-19 23:08:11 +0200

    Update repo

---
+++ Contests/Codeforces Round 878 (Div.3)/D-Wooden Toy Festival/D_Wooden_Toy_Festival.cpp
@@ -0,0 +1,116 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef pair<double,double> PD;
+typedef vector<ll> VL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<vector<ll> > VVL;
+typedef vector<vector<PL> > VVPL;
+typedef vector<PL> VPL;
+typedef vector<PI> VPI;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define sort(v) sort(v.begin(),v.end())
+#define f(start,i,end) for(int i = start; i < end; i++)
+#define lower(v,val) (lower_bound(v.begin(), v.end(), val) - v.begin())
+#define upper(v,val) (upper_bound(v.begin(), v.end(), val) - v.begin())
+// MODULAR DIVISION
+ll get_pow_mod(ll n, ll x){
+    ll ret = 1;
+    while(x){
+      if(x&1)
+       (ret*=n)%=MOD;
+      x>>=1;
+      (n*=n)%=MOD;
+     }
+     return ret;
+}
+ll get_mod_inverse(ll b){
+    return get_pow_mod(b,MOD-2);
+}
+ll modDivide(ll a, ll b){
+    ll inverse = get_mod_inverse(b);
+    return a % MOD * inverse % MOD;
+}
+
+
+
+// SOLVE
+void solve(){
+
+    int n; cin >> n;
+    VL toys(n);
+    f(0,i,n) cin >> toys[i];
+
+    sort(toys);
+
+    if(n <= 3){
+        cout << 0 << endl;
+        return;
+    }
+
+
+    ll low = 0;
+    ll high = toys[n-1] - toys[0];
+
+    ll best = high;
+
+    while(low <= high){
+
+        ll mid = (low + high) / 2;
+        ll thr1 = toys[0] + mid;
+        ll thr2 = toys[n-1] - mid;
+
+        ll ex1 = upper(toys, thr1 + mid);
+        ll ex2 = lower(toys, thr2 - mid) - 1;
+
+        if((ex1 >= ex2) or (toys[ex2] - toys[ex1] <= 2 * mid)){
+            best = mid;
+            high = mid - 1;
+        }
+
+        else{
+            low = mid + 1;
+        }
+    }
+    cout << best << endl;
+
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout<< "TESTCASE:  " <<  it << endl;
+        solve();
+    }
+    return 0;
+}
--- Contests/Codeforces Round 878 (Div.3)/README.md
+++ Contests/Codeforces Round 878 (Div.3)/README.md
@@ -5,3 +5,4 @@
 |  A | Cipher Shifer            | Two Pointers                        |
 |  B | Binary Cafe              | Combinatorics, Math                     |
 |  C | Ski Resort        | Combinatorics, Two Pointers                    |
+|  D | Wooden Toy Festival        | Binary Search, Greedy, Sortings                   |
