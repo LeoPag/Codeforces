b575b161624dab59059f6a5d583ee48b00300c2f
lpagan <leonardo.pagani99@gmail.com>
2023-08-09 00:59:44 +0200

    Update repo

---
+++ Contests/Codeforces Round 891 (Div.3)/F-Sum and Product/F_Sum_and_Product.cpp
@@ -0,0 +1,128 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef pair<double,double> PD;
+typedef vector<ll> VL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<PI> VPI;
+typedef vector<VPI> VVPI;
+typedef vector<vector<ll> > VVL;
+typedef vector<vector<PL> > VVPL;
+typedef vector<PL> VPL;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define sort(v) sort(v.begin(),v.end())
+#define f(start,i,end) for(int i = start; i < end; i++)
+#define f_rev(start,i,end) for(int i = start; i > end; i--)
+#define lower(v,val) (lower_bound(v.begin(), v.end(), val) - v.begin())
+#define upper(v,val) (upper_bound(v.begin(), v.end(), val) - v.begin())
+#define max_arr(v) *max_element(v.begin(), v.end())
+#define min_arr(v) *min_element(v.begin(), v.end())
+#define print(var) cout << var << endl
+#define print_vec(v) for (auto it = v.begin(); it != v.end(); it++) cout << *it << " "; cout << endl;
+#define print_pair(p) cout << p.first << " " << p.second << endl
+// MODULAR DIVISION
+ll get_pow_mod(ll n, ll x){
+    ll ret = 1;
+    while(x){
+      if(x&1)
+       (ret*=n)%=MOD;
+      x>>=1;
+      (n*=n)%=MOD;
+     }
+     return ret;
+}
+ll get_mod_inverse(ll b){
+    return get_pow_mod(b,MOD-2);
+}
+ll modDivide(ll a, ll b){
+    ll inverse = get_mod_inverse(b);
+    return a % MOD * inverse % MOD;
+}
+
+map<ll,ll> m;
+
+void solve_query(ll x, ll y){
+
+    long double discr = x*x - 4*y;
+    if (discr < 0){
+        cout << 0 << " ";
+        return;
+    }
+  
+    long double a1 =(x + sqrt(discr)) / 2;
+    long double a2 = (x - sqrt(discr)) / 2;
+  
+    if(floor(a1) != ceil(a1) or (floor(a2) != ceil(a2))){
+        cout << 0 << " ";
+        return;
+    }
+    if(a1 == a2){
+        cout << (m[a1] * (m[a1] - 1)) / 2  << " ";
+    }
+
+    else{
+        cout << (m[a1] * (m[a2]))  << " ";
+    }
+}
+
+// SOLVE
+void solve(){
+
+    int n; cin >> n;
+    m.clear();
+    VL a(n);
+
+    f(0,i,n) {
+        cin >> a[i];
+        m[a[i]] += 1;
+    }
+
+    int q; cin >> q;
+
+    f(0,i,q){
+        ll x,y; cin >> x >> y;
+        solve_query(x,y);
+    }
+    cout << endl;
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout<< "TESTCASE:  " <<  it << endl;
+        solve();
+        //if(it == 2) return 0;
+    }
+    return 0;
+}
