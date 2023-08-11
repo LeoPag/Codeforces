f8fa15c763afcdba13e6358efffbc0a2a6fbc1c0
lpagan <leonardo.pagani99@gmail.com>
2023-08-12 00:35:25 +0200

    Update repo

---
+++ Contests/Codeforces Round 858 (Div.2)/B-Mex Master/B_Mex_Master.cpp
@@ -0,0 +1,106 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef pair<double,double> PD;
+typedef vector<ll> VL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<PI> VPI;
+typedef vector<VPI> VVPI;
+typedef vector<vector<ll> > VVL;
+typedef vector<vector<PL> > VVPL;
+typedef vector<PL> VPL;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define sort(v) sort(v.begin(),v.end())
+#define f(start,i,end) for(int i = start; i < end; i++)
+#define f_rev(start,i,end) for(int i = start; i > end; i--)
+#define lower(v,val) (lower_bound(v.begin(), v.end(), val) - v.begin())
+#define upper(v,val) (upper_bound(v.begin(), v.end(), val) - v.begin())
+#define max_arr(v) *max_element(v.begin(), v.end())
+#define min_arr(v) *min_element(v.begin(), v.end())
+#define print(var) cout << var << endl
+#define print_vec(v) for (auto it = v.begin(); it != v.end(); it++) cout << *it << " "; cout << endl
+#define print_pair(p) cout << p.first << " " << p.second << endl
+// MODULAR DIVISION
+ll get_pow_mod(ll n, ll x){
+    ll ret = 1;
+    while(x){
+      if(x&1)
+       (ret*=n)%=MOD;
+      x>>=1;
+      (n*=n)%=MOD;
+     }
+     return ret;
+}
+ll get_mod_inverse(ll b){
+    return get_pow_mod(b,MOD-2);
+}
+ll modDivide(ll a, ll b){
+    ll inverse = get_mod_inverse(b);
+    return a % MOD * inverse % MOD;
+}
+
+// SOLVE
+void solve(){
+    int n; cin >> n;
+    VI a(n);
+    int zeros = 0;
+    int ones = 0;
+    f(0,i,n) {
+        cin >> a[i];
+        if(a[i] == 0) zeros += 1;
+        if(a[i] == 1) ones += 1;
+    }
+
+    if(zeros == n)print(1);
+
+    else if(zeros <= n/2 + n%2) print(0);
+   
+    else if(zeros + ones == n){
+        print(2);
+    }
+    else{
+        print(1);
+    }
+
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout<< "TESTCASE:  " <<  it << endl;
+        solve();
+        //if(it == 2) return 0;
+    }
+    return 0;
+}
---
+++ Contests/Codeforces Round 858 (Div.2)/README.md
@@ -0,0 +1,6 @@
+## The Problems
+
+|  Letter | Title                     | Topics and Techniques                          |
+|---------|---------------------------|-----------------------------|
+|  A | Walking Master        | Geometry,Greedy, Math                     |
+|  B | Mex Master          |Greedy                       |
