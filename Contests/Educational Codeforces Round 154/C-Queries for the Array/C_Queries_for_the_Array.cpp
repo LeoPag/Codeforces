0de87157b2f3f641b83128087d637fe5390d8ad8
lpagan <leonardo.pagani99@gmail.com>
2023-09-06 17:01:53 +0200

    Update repo

---
+++ Contests/Educational Codeforces Round 154/C-Queries for the Array/C_Queries_for_the_Array.cpp
@@ -0,0 +1,133 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+#include <bitset>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PI;
+typedef pair<ll,ll> PL;
+typedef pair<double,double> PD;
+typedef vector<ll> VL;
+typedef vector<int> VI;
+typedef vector<vector<int> > VVI;
+typedef vector<PI> VPI;
+typedef vector<VPI> VVPI;
+typedef vector<vector<ll> > VVL;
+typedef vector<vector<PL> > VVPL;
+typedef vector<PL> VPL;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define sort(v) sort(v.begin(),v.end())
+#define f(start,i,end) for(int i = start; i < end; i++)
+#define f_rev(start,i,end) for(int i = start; i > end; i--)
+#define lower(v,val) (lower_bound(v.begin(), v.end(), val) - v.begin())
+#define upper(v,val) (upper_bound(v.begin(), v.end(), val) - v.begin())
+#define max_arr(v) *max_element(v.begin(), v.end())
+#define min_arr(v) *min_element(v.begin(), v.end())
+#define print(var) cout << var << endl
+#define print_vec(v) for (auto it = v.begin(); it != v.end(); it++) cout << *it << " "; cout << endl
+#define print_pair(p) cout << p.first << " " << p.second << endl
+// MODULAR DIVISION
+ll get_pow_mod(ll n, ll x){
+    ll ret = 1;
+    while(x){
+      if(x&1)
+       (ret*=n)%=MOD;
+      x>>=1;
+      (n*=n)%=MOD;
+     }
+     return ret;
+}
+ll get_mod_inverse(ll b){
+    return get_pow_mod(b,MOD-2);
+}
+ll modDivide(ll a, ll b){
+    ll inverse = get_mod_inverse(b);
+    return a % MOD * inverse % MOD;
+}
+
+// SOLVE
+void solve(){
+    string s; cin >> s;
+
+    int n = s.size();
+    int c = 0;
+    int last_ord = 0;
+    VI prefix_ord(n+1,-1);
+
+    //0 means unordered
+    //1 means ordered
+    //-1 means not established yet
+
+    f(0,i,n){
+
+        if(s[i] == '+'){
+            c += 1;
+            if(c == 1) prefix_ord[c] = 1;
+        }
+        if(s[i] == '-'){
+            prefix_ord[c] = -1;
+            c -= 1;
+            if(c == 1) prefix_ord[c] = 1;
+            last_ord = min(last_ord, c);
+        }
+        if(s[i] == '0'){
+            if(c <= 1){
+                print("NO");
+                return;
+            }
+            if(prefix_ord[c] == 1){
+                print("NO");
+                return;
+            }
+            prefix_ord[c] = 0;
+        }
+        if(s[i] == '1'){
+            f(last_ord,j,c+1){
+                if(prefix_ord[j] == 0){
+                    print("NO");
+                    return;
+                }
+                prefix_ord[j] = 1;
+            }
+            last_ord = c;
+        }
+
+    } 
+    print("YES");
+
+}
+int main()
+{
+    fast_cin();
+    ll t;
+    cin >> t;
+    for(int it=1;it<=t;it++) {
+        //cout<< "TESTCASE:  " <<  it << endl;
+        solve();
+        //if(it == 2) return 0;
+    }
+    return 0;
+}
